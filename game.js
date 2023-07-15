e697be1d67dfbae1eda4513fbc33449f7a2a9d72
Tawksy <99350169+Tawksy@users.noreply.github.com>
2023-07-15 17:22:57 +0200

    Update 2
    Added computer-choice
    Added functioning User-Input check

--- game.js
+++ game.js
@@ -1,15 +1,25 @@
 console.log('Welcome to Rock, Paper, Scissors')
 
 const playableHands = ["rock", "paper", "scissors"];
+let computerChoice = playableHands[Math.floor(Math.random() * playableHands.length)]
 
-let playerHand = prompt("Enter your hand: ").toLowerCase(); 
-console.log(playerHand);
+
+
+let playerHand = prompt("Enter your hand: ").toLowerCase();
+
+if (playableHands.includes(playerHand)) {
+    console.log("Your choice: ", playerHand)
+    console.log("Computer choice: ", computerChoice)
+} else {
+    console.log("Invalid entry. Choose from rock, paper or scissors")
+}
+
+
+
 
 
 
 function getComputerChoice() { 
-    playableHands[(Math.floor(Math.random() * playableHands.3))];
 }
 
 getComputerChoice() 
-console.log(playableHands);
